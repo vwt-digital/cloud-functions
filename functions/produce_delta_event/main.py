a1b7ad3c7964246a9296ffde628052c9af809e10
Bernie van Veen <b.vanveen@vwt.digital>
2019-10-24 22:04:21 +0200

    Implement FILEPATH_PREFIX_FILTER. DAT-1067

--- functions/produce_delta_event/main.py
+++ functions/produce_delta_event/main.py
@@ -26,6 +26,7 @@ def gather_publish_msg(msg):
 
 def publish_json(msg, rowcount, rowmax, topic_project_id, topic_name):
     topic_path = publisher.topic_path(topic_project_id, topic_name)
+    # logging.info(f'Publish to {topic_path}: {msg}')
     future = publisher.publish(
         topic_path, bytes(json.dumps(msg).encode('utf-8')))
     future.add_done_callback(
@@ -144,64 +145,69 @@ def publish_diff(data, context):
     filename = data['name']
     df_new = None
 
-    try:
-        # Read dataframe from store
-        df_new = df_from_store(bucket, filename)
-        # Get previous data from archive
-        blob_prev = get_prev_blob(config.ARCHIVE)
-        full_load = config.FULL_LOAD if hasattr(config, 'FULL_LOAD') else False
+    prefix_filter = config.FILEPATH_PREFIX_FILTER if hasattr(config, 'FILEPATH_PREFIX_FILTER') else None
 
-        # Read previous data from archive and compare
-        if blob_prev and (not full_load):
-            df_prev = df_from_store(config.ARCHIVE, blob_prev)
-            cols_prev = set(df_prev.columns)
-            cols_new = set(df_new.columns)
-            # When there are different columns in new file with respect to the old one,
-            # the file is rejected.
-            if cols_prev != cols_new:
-                diff = cols_new - cols_prev
-                raise ValueError("Different schema: {} '{}' found in new delivery but not in previous delivery".format(
-                    'column' if len(diff) == 1 else 'columns',
-                    "', '".join(list(diff)),
-                ))
-            df_diff = calculate_diff(df_prev, df_new)
-        else:
-            df_diff = df_new.copy().drop_duplicates()
+    if not prefix_filter or filename.startswith(prefix_filter):
+        try:
+            # Read dataframe from store
+            df_new = df_from_store(bucket, filename)
+            # Get previous data from archive
+            blob_prev = get_prev_blob(config.ARCHIVE)
+            full_load = config.FULL_LOAD if hasattr(config, 'FULL_LOAD') else False
 
-        # Check the number of new records
-        # In case of no new records: don't send any updates
-        if len(df_diff) == 0:
-            logging.info('No new rows found')
-        else:
-            logging.info('Found {} new rows.'.format(len(df_diff)))
+            # Read previous data from archive and compare
+            if blob_prev and (not full_load):
+                df_prev = df_from_store(config.ARCHIVE, blob_prev)
+                cols_prev = set(df_prev.columns)
+                cols_new = set(df_new.columns)
+                # When there are different columns in new file with respect to the old one,
+                # the file is rejected.
+                if cols_prev != cols_new:
+                    diff = cols_new - cols_prev
+                    raise ValueError("Different schema: {} '{}' found in new delivery but not in previous delivery".format(
+                        'column' if len(diff) == 1 else 'columns',
+                        "', '".join(list(diff)),
+                    ))
+                df_diff = calculate_diff(df_prev, df_new)
+            else:
+                df_diff = df_new.copy().drop_duplicates()
 
-            # Export to json
-            rows_str = df_diff.to_json(orient='records')
-            rows_json = json.loads(rows_str)
+            # Check the number of new records
+            # In case of no new records: don't send any updates
+            if len(df_diff) == 0:
+                logging.info('No new rows found')
+            else:
+                logging.info('Found {} new rows.'.format(len(df_diff)))
 
-            # Publish individual rows to topic
-            i = 1
-            for row in rows_json:
-                publish_json(gather_publish_msg(row), rowcount=i, rowmax=len(rows_json), **config.TOPIC_SETTINGS)
-                i += 1
+                # Export to json
+                rows_str = df_diff.to_json(orient='records')
+                rows_json = json.loads(rows_str)
 
-        if config.INBOX != config.ARCHIVE:
-            # Write file to archive
-            df_to_store(config.ARCHIVE, filename, df_new)
-            # Remove file from inbox
-            remove_from_store(config.INBOX, filename)
+                # Publish individual rows to topic
+                i = 1
+                for row in rows_json:
+                    publish_json(gather_publish_msg(row), rowcount=i, rowmax=len(rows_json), **config.TOPIC_SETTINGS)
+                    i += 1
 
-        logging.info('Run succeeded')
+            if config.INBOX != config.ARCHIVE:
+                # Write file to archive
+                df_to_store(config.ARCHIVE, filename, df_new)
+                # Remove file from inbox
+                remove_from_store(config.INBOX, filename)
 
-    except Exception as e:
-        if hasattr(config, 'ERROR'):
-            df_to_store(config.ERROR, filename, df_new)
-        if config.INBOX != config.ARCHIVE:
-            remove_from_store(config.INBOX, filename)
-        logging.error('Processing failure {}'.format(e))
-        raise
-    finally:
-        logging.info('Run done')
+            logging.info('Run succeeded')
+
+        except Exception as e:
+            if hasattr(config, 'ERROR'):
+                df_to_store(config.ERROR, filename, df_new)
+            if config.INBOX != config.ARCHIVE:
+                remove_from_store(config.INBOX, filename)
+            logging.error('Processing failure {}'.format(e))
+            raise
+        finally:
+            logging.info('Run done')
+    else:
+        logging.info(f'Skipping {filename} due to FILEPATH_PREFIX_FILTER {prefix_filter}')
 
 
 # main defined for testing
